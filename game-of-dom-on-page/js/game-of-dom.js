f074c58bb76452abe0b83f1c2559a174863807e3
=-kriss-= <kriss@modelcitizen.com.au>
2014-05-17 15:57:10 +1000

    successfully removed canvas, using css clone isntead

--- game-of-dom-on-page/js/game-of-dom.js
+++ game-of-dom-on-page/js/game-of-dom.js
@@ -1,10 +1,80 @@
-(function ( $ ) {
+/*
+ * getStyleObject Plugin for jQuery JavaScript Library
+ * From: http://upshots.org/?p=112
+ *
+ * Copyright: Unknown, see source link
+ * Plugin version by Dakota Schneider (http://hackthetruth.org)
+ */
+
+(function($){
+  $.fn.getStyleObject = function(){
+    var dom = this.get(0);
+    var style, prop;
+    var returns = {};
+    if(window.getComputedStyle){
+      var camelize = function(a,b){
+        return b.toUpperCase();
+      };
+      style = window.getComputedStyle(dom, null);
+      for(var i=0;i<style.length;i++){
+        prop = style[i];
+        var camel = prop.replace(/\-([a-z])/g, camelize);
+        var val = style.getPropertyValue(prop);
+        returns[camel] = val;
+      }
+      return returns;
+    }
+    if(dom.currentStyle){
+      style = dom.currentStyle;
+      for(prop in style){
+          returns[prop] = style[prop];
+      }
+      return returns;
+    }
+    return this.css();
+  };
+  $.fn.cloneWithCSS = function() {
+    styles = {};
+
+    $this = $(this);
+    $clone = $this.clone();
+    $clone.css( $this.getStyleObject() );
+
+    children = $this.children().toArray();
+    var i = 0;
+    while( children.length ) {
+      $child = $( children.pop() );
+      styles[i++] = $child.getStyleObject();
+      $child.children().each(function(i, el) {
+          children.push(el);
+      });
+    }
+
+    var cloneChildren = $clone.children().toArray();
+    var i = 0;
+    while( cloneChildren.length ) {
+      $child = $( cloneChildren.pop() );
+      $child.css( styles[i++] );
+      $child.children().each(function(i, el) {
+          cloneChildren.push(el);
+      });
+    }
+
+    return $clone;
+  };
 
 //------------------------------------------------------------------------//
 // create a canvas element that duplocates the element selected
 //------------------------------------------------------------------------//
  
-  $.fn.dupliCanvas = function (snapChat) {
+  $.fn.dupliCanvas = function () {
+
+    $clone = this.cloneWithCSS();
+    $clone.css({
+      'position' : 'absolute', 
+      'left': 0, 
+      'top' : 0
+    });
 
     // get selected element x,y pos and width,height
     var winScrollLeft = $(window).scrollLeft();   
@@ -14,64 +84,29 @@
     var canvasX = this.offset().left - winScrollLeft;
     var canvasY = this.offset().top - winScrollTop;
 
-    console.log(snapChat)
-
-    html2canvas(document.getElementById(snapChat), {
-      onrendered: function(canvas) {
-        winBody.append(canvas);
-        var newCanvas = winBody.find('canvas').last();
-        newCanvas.addClass('divFighter');
-        newCanvas.css({
-          'left' : canvasX,
-          'top' : canvasY
-        });
-        if ($('.divFighter').length == 1) {
-          newCanvas.attr('id', 'TeTOne');
-        } else {
-          newCanvas.attr('id', 'TeTTwo');
-        };
-
-      },
-      width: canvasWidth,
-      height: canvasHeight
-    });
-
-    
-
-    
-
     // create duplicate element - named diffently
-    // if ($('.divFighter').length === 0) {
-    //   var traceElement = '<canvas id="TEOne" class="divFighter" />';
-    //   winBody.append(traceElement);
-    //   var $elem = $('#TEOne')
-    // } else {
-    //   traceElement = '<canvas id="TETwo" class="divFighter" />';
-    //   winBody.append(traceElement);
-    //   $elem = $('#TETwo')
-    // };
+    var traceElement, $elem;
+
+    if (!$('.divFighter').length) {
+      traceElement = '<div id="TEOne" class="divFighter" />';
+      winBody.append(traceElement);
+      $elem = $('#TEOne');
+    } else {
+      traceElement = '<div id="TETwo" class="divFighter" />';
+      winBody.append(traceElement);
+      $elem = $('#TETwo');
+    };
     
     // make dupliate match size/pos 
-    // $elem.css({
-    //   //'width' : canvasWidth,
-    //   //'height' : canvasHeight,
-    //   'left' : canvasX,
-    //   'top' : canvasY
-    // }).attr({
-    //   width: canvasWidth,
-    //   height: canvasHeight
-    // });
+    $elem.css({
+      'width' : canvasWidth,
+      'height' : canvasHeight,
+      'left' : canvasX,
+      'top' : canvasY
+    });
 
-    // now make the canvas look like the original element
-    // var context = $elem.get(0).getContext("2d");
-    // domvas.toImage(this.get(0), function() {
-    //   context.drawImage(this, 0, 0);
-    // });
-
-    // add style to chosen elem
-    //this.addClass('divReplaced')
-    var divContent = this.html();
-
+    //now make the new element look like the original element
+    $elem.append($clone); 
   };
 } ( jQuery ) );
 
@@ -104,37 +139,34 @@ var domGame = {
     $('head').append('<link rel="stylesheet" type="text/css" href="' + projectHost + 'css/game-of-dom.css">'); 
 
     currentMsg = 'welcome';
+    popUp.messageCreateContainer();
     popUp.messageFetch(currentMsg);
+    popCrowd.initialize();
 
     this.listen();
 
   },
+
   listen : function () {
     // register a click on any elem
     $(document).click( function (event) {
       event.preventDefault();
 
-      // need to set up uniq id to use
-      var snapChat
-      if ($('#hotdogs').length == 0) {
-        snapChat = 'hotdogs';
-      } else {
-        snapChat = 'butterfly';
-      };
-
-      // add id to selected id if not already
-      if ($(event.target).is('[id]')){
-        snapChat = $(event.target).attr('id');
-      } else {
-        $(event.target).attr('id', snapChat);
-      }
-
       // stop listeing if 2 elems already selected
-      if ($('.divFighter').length < 2) {
+      if (!$('#TETwo').length) {
         console.log('clicked on ', event.target);
-        $(event.target).dupliCanvas(snapChat);
+        $(event.target).dupliCanvas();
+        $(event.target).addClass('domCloned');
+      };
+      if ($('#TEOne').length){
+        setTimeout(domGame.stage(), 500)
       };
     });
+  },
+
+  stage : function () {
+    popCrowd.initialize();
+
   }
 };
 
@@ -152,7 +184,7 @@ var popUp = {
   },
   
   messageFetch : function (currentMsg) {
-    var url = (projectHost + messagePath + currentMsg + '/json.txt')
+    var url = (projectHost + messagePath + currentMsg + '/json.txt');
     $.ajax({
       type: 'GET',
       dataType: 'json',
@@ -162,7 +194,7 @@ var popUp = {
         popUp.messageShow(ajaxData.html);
       },
       error : function(){
-        console.log('ajax popUp.messageFetch(' + currentMsg + ') error')
+        console.log('ajax popUp.messageFetch(' + currentMsg + ') error');
         popUp.messageShow('error');
       }
     });
@@ -182,16 +214,27 @@ var popUp = {
     // check if stylesheet loaded before running fadeIn()
     if ($('#sanityTest').is(':hidden')) {
       $GDMessageContainer.fadeIn(1000);
-      clearInterval(reload)
+      clearInterval(reload);
     } else {
       reload = setTimeout(function(){
-        console.log('stylesheet not loaded yet - retry')
-        popUp.messageShow(currentMsg)
+        console.log('stylesheet not loaded yet - retry');
+        popUp.messageShow(currentMsg);
       }, 500);
     };
     $GDMessageContainer.append(currentMsg);
   }
 };
+
+var popCrowd = {
+  initialize : function () {
+    winBody.append('<div id="domCrowd" />');
+    $domCrowd = $('#domCrowd');
+    $domCrowd.attr({
+      'width' : winWidth,
+      'height' : winHeight, 
+    })
+  }
+}
 
 
 
